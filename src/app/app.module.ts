f357586184b4585c223361d2a56e34cfd975f9c5
Aditya-Raj-Taazaa <aditya.raj@taazaa.com>
2025-05-20 15:08:37 +0530

    First Commit to UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { BookAuthorFeedComponent } from './book-author-feed/book-author-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    BookAuthorFeedComponent
   ],
   imports: [
     BrowserModule
